68461a51cd89f705a2f33e71aeef8f2e59adbcc0
HungNguyenPQ <quyhung6129@gmail.com>
2023-08-15 16:11:44 +0700

    Update images asset adding new banner

--- lib/core/app_asset.dart
+++ lib/core/app_asset.dart
@@ -1,10 +1,18 @@
 class AppAsset {
-  static const String bubbleTea = 'assets/bubble-tea.png';
-  static const String friedChicken = 'assets/fried-chicken.png';
-  static const String iceCream = 'assets/ice-cream.png';
-  static const String noddle = 'assets/pho.png';
-  static const String pizza = 'assets/pizza.png';
-  static const String salad = 'assets/salad.png';
-  static const String steak = 'assets/steak.png';
-  static const String foodBanner = 'assets/food-banner.png';
+  // food menu
+  static const String bubbleTea = 'assets/food/bubble-tea.png';
+  static const String friedChicken = 'assets/food/fried-chicken.png';
+  static const String iceCream = 'assets/food/ice-cream.png';
+  static const String noddle = 'assets/food/pho.png';
+  static const String pizza = 'assets/food/pizza.png';
+  static const String salad = 'assets/food/salad.png';
+  static const String steak = 'assets/food/steak.png';
+
+  // food banner
+  static const String foodBanner1 = "assets/banner/food-banner-1.png";
+  static const String foodBanner2 = "assets/banner/food-banner-2.png";
+  static const String foodBanner3 = "assets/banner/food-banner-3.png";
+  static const String foodBanner4 = "assets/banner/food-banner-4.png";
+  static const String foodBanner5 = "assets/banner/food-banner-5.png";
+  static const String foodBanner6 = "assets/banner/food-banner-6.png";
 }
