bf75363f0763d52a3e8d77c92f2bf0b59083fafc
HungNguyenPQ <quyhung6129@gmail.com>
2023-08-17 00:17:53 +0700

    Add authen pages

---
+++ lib/presentation/authenticate/signin/sign_in_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ihun_food_app/config/styles/palettes.dart';
+import 'package:ihun_food_app/core/widgets/app_btn_label.dart';
+import 'package:ihun_food_app/presentation/authenticate/widgets/app_txtfield.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palettes.p2,
+      bottomSheet: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.r),
+              topRight: Radius.circular(20.r),
+            ),
+          ),
+          height: MediaQuery.of(context).size.height * 0.8,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AppTextFeild(
+                controller: emailController,
+                lblText: 'Type in your email',
+                iconData: Icons.email,
+                onChange: (value) {},
+              ),
+              AppTextFeild(
+                controller: passwordController,
+                txtfType: 'password',
+                lblText: 'Type in your password',
+                iconData: Icons.password_rounded,
+                onChange: (value) {},
+              ),
+              AppBtnLabel(
+                label: 'Log in',
+                labelColor: Palettes.textWhite,
+                bgColor: Palettes.textBlack,
+                iconData: Icons.login,
+                iconColor: Palettes.textWhite,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.r),
+                ),
+                onPressed: () {},
+              )
+            ],
+          )),
+    );
+  }
+}
